6f1a69c4ab2f1f67b1986f711286ff299a7cc7e6
Frankie <frankidatank@gmail.com>
2023-09-13 20:50:43 -0700

    add user logout logic and routes

--- internal/models/users.go
+++ internal/models/users.go
@@ -51,7 +51,33 @@ func (m *UserModel) Insert(name, email, password string) error {
 // the provided email address and password. This will return the relevant
 // user ID if they do.
 func (m *UserModel) Authenticate(email, password string) (int, error) {
-	return 0, nil
+	var id int
+	var hashedPassword []byte
+
+	stmt := "SELECT id, hashed_password FROM users WHERE email = ?"
+
+	err := m.DB.QueryRow(stmt, email).Scan(&id, &hashedPassword)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	// We have found a user with given email, now we check if password matches
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	//password matches return id
+	return id, nil
+
 }
 
 // We'll use the Exists method to check if a user exists with a specific ID.
